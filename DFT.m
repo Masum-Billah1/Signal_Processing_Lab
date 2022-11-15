69a6dcdb941871d044e3e31dad2d1380589101b8
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2022-11-16 00:11:33 +0600

    Update DFT.m

--- DFT.m
+++ DFT.m
@@ -7,37 +7,50 @@ ts=1/fs;
 N=8;
 n=0:N-1;
 x=sin(2*pi*1000*n*ts) + 0.5*sin(2*pi*2000*n*ts + 3*pi/4);
-%x = [1 2 3 4 5 6 7 8]
 figure(1)
+subplot(1,2,1);
 stem(n,x);
+title('Main input');
+%x = [1 2 3 4 5 6 7 8]
 X = zeros(1,N);
 for m = 1:N
     for n = 1:N
-        X(m) = X(m) + x(n)*exp(-2j*pi*(n-1)*(m-1)/N);
+        X(m) = X(m) + x(n)*exp(-2*j*pi*(n-1)*(m-1)/N);
     end
 end
 n = 0:N-1;
 rX = real(X);
 iX = imag(X);
+figure(2);
 subplot(3,2,1);
-plot(n,rX);
+stem(n,rX);
 title('real part of DFT');
 subplot(3,2,2);
-plot(n,iX);
+stem(n,iX);
 title('imaginary part of DFT');
+subplot(3,2,3);
+stem(n,iX);
+title('imaginary part of DFT');
+subplot(3,2,4);
+stem(n,abs(X));
+title('Magnitude');
+y = abs(X).^2;
+subplot(3,2,5)
+stem(n,y);
+title('Power spectrum');
+
 ln=length(X);
 x=zeros(1,ln);
 
 for i=1:ln
     for n=1:ln
-        x(i)=x(i)+((1/ln)*X(n)*exp(2*pi*j*(i-1)*(n-1)/ln));
+        x(i)=x(i)+(X(n)*exp(2*pi*j*(i-1)*(n-1)/ln));
     end
+    x(i)=(1/ln)*x(i);
 end
-subplot(3,2,3);
+n=0:N-1;
 rx = real(x);
-ix = imag(x);
-plot(rx);
+figure(1);
+subplot(1,2,2);
+stem(n,rx);
 title('real part of Inverse DFT');
-subplot(3,2,4);
-plot(ix);
-title('imaginary part of Inverse DFT');
