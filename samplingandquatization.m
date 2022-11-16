4665fd401127c8a3910fcc88744a058c7ad34b01
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2022-11-16 08:10:34 +0600

    Update samplingandquatization.m

--- samplingandquatization.m
+++ samplingandquatization.m
@@ -1,6 +1,7 @@
 clc;
 clear all;
 close all;
+
 a1 = 10;
 a2 = 20;
 a3 = 40;
@@ -14,39 +15,38 @@ n = 1000;
 t = 0:1/n:1;
 
 x = a1*sin(2*pi*f1*t) + a2*sin(2*pi*f2*t) + a3*sin(2*pi*f3*t);
+
 subplot(2,1,1);
-plot(t,x,'red');
+plot(t,x,'r');
 hold on;
 
 fos = 40;
 tos = 0:1/fos:1;
-x1 = a1*sin(2*pi*f1*tos) + a2*sin(2*pi*f2*tos) + a3*sin(2*pi*f3*tos);
+x1 = a1*sin(2*pi*f1*tos) + a2*sin(2*pi*f2*tos) + a3*sin(2*pi*f3*tos);;
 subplot(2,1,1);
-plot(tos,x1,'green');
+plot(tos,x1,'b');
+hold on;
 
-fus = 20;%allising happend
+
+fus = 10;
 tus = 0:1/fus:1;
-x1 = a1*sin(2*pi*f1*tus) + a2*sin(2*pi*f2*tus) + a3*sin(2*pi*f3*tus);
+x2 = a1*sin(2*pi*f1*tus) + a2*sin(2*pi*f2*tus) + a3*sin(2*pi*f2*tus);
 subplot(2,1,1);
-plot(tus,x1,'blue');
+plot(tus,x2,'g');
 
-
-signal1 = x;
-nbit = 2;
-level = 2.^nbit;
-mn = min(a1,min(a2,a3));
+signal = x;
+n = 2;
+level = 2.^n;
 mx = max(a1,max(a2,a3));
-scallingfac = (mx-mn)/(level);
-signal1=signal1/scallingfac;
-signal1 = round(signal1);
-signal1 = signal1*scallingfac;
-subplot(2,1,2)
-plot(t,signal1,'r');
+mn = min(a1,min(a2,a3));
+scaling_fac = (mx-mn)/level;
+signal = signal/scaling_fac;
+signal = round(signal);
+signal = signal*scaling_fac;
+subplot(2,1,2);
+plot(t,signal,'r');
 hold on;
 
-signal1 = signal1 - x;
-subplot(2,1,2)
-plot(t,signal1,'g');
-hold on;
-
-
+error = signal-x;
+subplot(2,1,2);
+plot(t,error,'g');
